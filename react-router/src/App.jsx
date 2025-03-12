33df4702ed8641a486964f89474d2f437034c086
Mohamed Desoky <mae.desoky@gmail.com>
2025-03-11 22:02:56 -0400

    new routes added

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,14 +1,34 @@
 import { useState } from 'react'
+import { Routes, Route, Link} from 'react-router-dom'
 import './App.css'
+import Blue from './components/Blue.jsx'
+import Red from './components/Red.jsx'
+import Home from './components/Home.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div id="container">
+      {/* <h1>Hello React Router!</h1> */}
+      <div id="navbar">
+        <Link to="/blue">Blue</Link> | 
+        <Link to="/red">Red</Link>
+      </div>
+      
+      <div id="main-section">
 
-    </>
-  )
+        <Routes>
+          <Route path="/blue" element={<Blue />} />
+          <Route path="/red" element={<Red />} />   
+          <Route path="/" element={<Home />} /> 
+        </Routes>
+
+      </div>
+    </div>
+    
+  );
+
 }
 
 export default App
---
+++ react-router/src/components/Red.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Red() {
+  return (
+    <div className="red">
+      <h1>Red</h1>
+    </div>
+  );
+}
+
